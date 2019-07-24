47ee522daf4fd633c168b0bca78b142973a9e7f8
Christopher Thomas <chthomas@wiley.com>
2019-07-24 14:00:44 -0400

    Added GSON to dependancies. Wrote logic to ingest json string and deseralize into Java object.

--- src/main/java/com/example/dynatraceWH/WebhookController.java
+++ src/main/java/com/example/dynatraceWH/WebhookController.java
@@ -7,6 +7,60 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import com.google.gson.*;
+import java.util.List;
+
+
+class TestObject {
+    private String username;
+    private String password;
+    TestObject() {
+
+    }
+}
+
+// dynatrace object to be constructed from JSON string 
+class DynatraceJavaObject {
+
+    public List<ImpactedEntity> ImpactedEntities = null;
+    public String ImpactedEntity;
+    public String PID;
+    public String ProblemDetailsHTML;
+    public ProblemDetailsJSON ProblemDetailsJSON;
+    public String ProblemID;
+    public String ProblemImpact;
+    public String ProblemTitle;
+    public String ProblemURL;
+    public String State;
+    public String Tags;
+
+    DynatraceJavaObject() {
+
+    }
+
+}
+
+class ImpactedEntity {
+
+    public String type;
+    public String name;
+    public String entity;
+
+    ImpactedEntity() {
+
+    }
+
+}
+
+class ProblemDetailsJSON {
+
+    public String ID;
+
+    ProblemDetailsJSON() {
+
+    }
+
+}
 
 @Controller
 public class WebhookController {
@@ -14,14 +68,16 @@ public class WebhookController {
     @GetMapping("/call")
     @ResponseBody
     public String testResponse(){
-         return "Test Response for WebHook Listener";
+        return "Test Response for WebHook Listener";
     }
 
     // use this to post dynatrace hooks to Listener 
     @PostMapping("/JSON")
     @ResponseBody
     public String jsonResponse(@RequestBody String msg){
-
-         return msg;
+        Gson g = new Gson();
+        DynatraceJavaObject p = g.fromJson(msg, DynatraceJavaObject.class);
+        String json = g.toJson(p);  
+        return json;
     }
 }
